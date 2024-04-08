00009842c95457b896b3f9951c45e263583685d9
missymae#2783 <141260118+myCatsName@users.noreply.github.com>
2024-04-08 10:24:15 -0600

    adjust main theme rate add camel noise

--- src/sound/music.js
+++ src/sound/music.js
@@ -1,27 +1,43 @@
 import { Howl } from "howler"
 import acousticMP3 from "../sound/acoustic-guitar-by-skiwyy-14k.mp3"
 import bushWalkMP3 from "../sound/bushwalk-by-jecky-14k.mp3"
+import camelMP3 from "../sound/annakstedt_camel14k.mp3"
 
-export const acousticFast = new Howl({
+ const acousticFast = new Howl({
     src: [acousticMP3],
     volume:0.05,
-    rate:1,
+    rate:.8,
     loop:true
 })
-export const acousticSlow = new Howl({
+ const acousticSlow = new Howl({
     src: [acousticMP3],
-    volume:0.25,
-    rate:0.8,
+    volume:0.15,
+    rate:0.7,
     loop:true
 })
-export const bushWalkSounds = new Howl({
+ const bushWalkSounds = new Howl({
     src: [bushWalkMP3],
-    volume:1.25,
+    volume:1.45,
     rate:0.8,
     loop:true
 })
+
+const camelSound = new Howl({
+src: [camelMP3],
+sprite: {
+    grunt1: [0,4000],
+    grunt2: [6000,6000]
+}
+})
+
 export function playMainTheme(){
     acousticFast.play();
     acousticSlow.play();
     bushWalkSounds.play();
+}
+
+export function playCamelSoundAtRandom(){
+    Math.random() < .5
+    ? camelSound.play('grunt1')
+    : camelSound.play('grunt2')
 }
